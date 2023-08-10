be21d8fa3e3dd0f63a0515ccffc1b1bfe60d3e9b
zhyee <1054948153@qq.com>
2023-08-10 21:45:46 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(DDTRACE_HOME="$(pwd)/build/datadog"; 
+(DDTRACE_HOME="$(pwd)/bin/Release/net7.0/datadog"; \ 
 CORECLR_ENABLE_PROFILING=1 \
 CORECLR_PROFILER="{846F5F1C-F9AE-4B07-969E-05C26BC060D8}" \
 CORECLR_PROFILER_PATH="$DDTRACE_HOME/linux-x64/Datadog.Trace.ClrProfiler.Native.so" \
@@ -16,4 +16,4 @@ DD_PROFILING_GC_ENABLED=1 \
 LD_PRELOAD="$DDTRACE_HOME/linux-x64/Datadog.Linux.ApiWrapper.x64.so" \
 DD_SERVICE=dotnet-profiling-demo DD_ENV=testing DD_VERSION=1.2.3 \
 DD_AGENT_HOST=127.0.0.1 DD_TRACE_AGENT_PORT=9529 \
-build/dotnet-profiling-demo)
+dotnet bin/Release/net7.0/dotnet-profiling-demo.dll)
